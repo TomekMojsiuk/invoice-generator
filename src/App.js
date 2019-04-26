05137a8359d5d0bc66bba08b32dbf0590b2e61e2
TomekMojsiuk <36561875+TomekMojsiuk@users.noreply.github.com>
2019-04-26 12:51:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ class App extends Component {
 					<BackgroundImg/>
 
 					<TopNav handleLogOut={this.handleLogOut} isLoggedIn={this.state.isLoggedIn}/>
-					{!this.state.isLoggedIn ? (
+					{this.state.isLoggedIn ? (
 
 						<Switch>
 
